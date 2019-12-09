fb9a1d9764314f46203843305b8cfc91976d6d94
buildanddestroy <devreap1@gmail.com>
2019-12-08 23:17:15 -0700

    Add notation for bash functions

--- secure_rpi.sh
+++ secure_rpi.sh
@@ -5,14 +5,14 @@
 # Secure the Raspberry Pi #
 ###########################
 
-function run_as_root() {
+function run_as_root() { #  Best to run as root
     if [[ $(id -u) != 0 ]]; then
         echo '[*] Must be ran as root.'
         exit
     fi
 }
 
-function create_pi_password () {
+function create_pi_password() { #  Create a new pi user password
     echo '[*] Type in user "pi" password.'
     read -sp "Password: " password_pi_variable
 }
@@ -22,7 +22,7 @@ function set_pi_password() { #  Set the new password for the pi user.
     echo -e "$password_pi_variable\n$password_pi_variable\n" | sudo passwd pi
 }
 
-function create_root_password() {
+function create_root_password() { #  Ask for the users new root password
     echo '[*] Type in user "root" password.'
     read -sp "Password: " password_root_variable
 }
@@ -32,24 +32,24 @@ function set_root_password() { #  Set the new password for the root user.
     echo -e "$password_root_variable\n$password_root_variable\n" | sudo passwd root
 }
 
-function create_heateduser_password() {
+function create_heateduser_password() { #  Ask for the new user password for heateduser
     echo '[*] Type in user "heateduser" password.'
     read -sp "Password: " password_heateduser_variable
 }
 
-function creat_heateduser_user() {
+function creat_heateduser_user() { #  Set the new password for heateduser
     echo -e '[*] Creating user heateduser'
     sudo /usr/sbin/useradd heateduser -m -s /bin/bash -U
     echo -e "$password_heateduser_variable\n$password_heateduser_variable\n" | passwd heateduser
     sudo usermod -a -G adm,dialout,cdrom,sudo,audio,video,plugdev,games,users,input,netdev,gpio,i2c,spi heateduser
 }
 
-function set_keyboard_english() {
+function set_keyboard_english() { #  Update keyboard to English US
     echo -e '[*] Setting Keyboard to English (US)'
     sed -i 's/gb/us/g' /etc/default/keyboard
 }
 
-function secure_sudo() {
+function secure_sudo() { #  Require password when using sudo for pi and heateduser
     sed -i 's/NOPASSWD/PASSWD/g' /etc/sudoers.d/010_pi-nopasswd
     mv /etc/sudoers.d/010_pi-nopasswd /etc/sudoers.d/010_pi-passwd
     touch /etc/sudoers.d/010_heateduser-passwd
@@ -57,7 +57,7 @@ function secure_sudo() {
     sed -i 's/pi/heateduser/g' /etc/sudoers.d/010_heateduser-passwd
 }
 
-function configure_sshd() {
+function configure_sshd() { #  Add a Banner to ssh requiring permission to get into the Pi.
     echo "" > /etc/issue.net
     echo "               #####################################################" >> /etc/issue.net
     echo "               # Unauthorized access to this machine is prohibited #" >> /etc/issue.net
@@ -74,18 +74,26 @@ function configure_sshd() {
     sudo systemctl restart ssh
 }
 
-function install_firewall() {
+function update_apt() { #  Update apt repo for up to date packaging
+    sudo apt-get update -y
+    sudo apt-get upgrade -y
+    sudo apt-get dist-upgrade -y
+    sudo apt-get autoclean -y
+    sudo apt-get autoremove -y
+}
+
+function install_firewall() { #  Install the firewall and allow port 22
     sudo apt-get install ufw -y
     sudo ufw enable
     sudo ufw allow 22/tcp
     sudo ufw status
 }
 
-function install_iptables() {
+function install_iptables() { #  Install persisent iptables
     sudo apt-get install iptables-persistent -y
 }
 
-function reboot_pi() {
+function reboot_pi() { #  Reboot the pi
     echo '[*] Rebooting the pi in 5 seconds.'
     sleep 5
     reboot
@@ -105,6 +113,7 @@ create_root_password
 set_root_password
 configure_sshd
 secure_sudo
+update_apt
 install_iptables
 install_firewall
 reboot_pi
